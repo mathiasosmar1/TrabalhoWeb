6d844e951a4ccb9e047994d8d4225dcb7283bf18
Mathias Osmar <mathias_os@hotmail.com>
2014-06-07 17:33:24 -0300

    Exportando trabalho para o gitHub

---
+++ TrabalhoWeb/src/Classes/Pessoa.java
@@ -0,0 +1,62 @@
+package Classes;
+
+public abstract class Pessoa {
+	
+	String nome;
+	String sexo;
+	String cpf;
+	String nu_telefone;
+	String nu_celular;
+	String de_endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNu_telefone() {
+		return nu_telefone;
+	}
+	public void setNu_telefone(String nu_telefone) {
+		this.nu_telefone = nu_telefone;
+	}
+	public String getNu_celular() {
+		return nu_celular;
+	}
+	public void setNu_celular(String nu_celular) {
+		this.nu_celular = nu_celular;
+	}
+	public String getDe_endereco() {
+		return de_endereco;
+	}
+	public void setDe_endereco(String de_endereco) {
+		this.de_endereco = de_endereco;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	String rg;
+	String email;
+}
